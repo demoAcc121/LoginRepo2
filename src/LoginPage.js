a1dc70e0643e96dcae5107bc9411dc32fe33964d
Rishi Kumar <rishichandra1415@gmail.com>
2025-07-16 21:28:41 +0530

    Initial commit

---
+++ src/LoginPage.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import "./index.css";
+import "./App.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+const LoginPage = () => {
+  const [email, SetEmail] = useState();
+  const [password, SetPassword] = useState();
+  const navigate = useNavigate();
+  const handleSubmit = () => {
+    const Data = {
+      username: email,
+      password: password,
+    };
+    //access  token and calling api...
+    axios
+      .post("https://dummyjson.com/auth/login", Data)
+      .then((res) => {
+        localStorage.setItem("token", res.data.accessToken);
+        alert("Login Successfull");
+        // console.log("login Succussfully", res.data);
+        navigate("/profile");
+      })
+      .catch((error) => {
+        alert("failed in login");
+        // console.log("login Failed", error);
+      });
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-400 to-blue-700">
+        <div className="space-y-4 p-10 rounded-md bg-white shadow-md w-96">
+          <h2 className="text-center text-2xl font-bold mb-4">Login Form</h2>
+          <div>
+            <p>Email</p>
+            <input
+              onChange={(e) => SetEmail(e.target.value)}
+              type="text"
+              className="border rounded-md shadow-md w-full p-2"
+              placeholder="email"
+              required
+            />
+          </div>
+          <div>
+            <p>Password</p>
+            <input
+              onChange={(e) => SetPassword(e.target.value)}
+              type="password"
+              className="border rounded-md shadow-md w-full p-2"
+              placeholder="password"
+              required
+            />
+          </div>
+          <button
+            onClick={handleSubmit}
+            className="bg-blue-700 text-white px-4 py-2 rounded-md w-full"
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ src/Show.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import LoginPage from "./LoginPage";
+const Show = () => {
+  const [userData, setUserData] = useState(null);
+  const navigate = useNavigate();
+  const getData = () => {
+    const token = localStorage.getItem("token");
+    const header = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    axios
+      .get("https://dummyjson.com/auth/me", header)
+      .then((res) => {
+        setUserData(res.data);
+      })
+      .catch((error) => {
+        alert("please enter details");
+        console.log(error);
+      });
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setUserData(null);
+    alert("Logout Successful");
+    navigate("/");
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-center space-x-4">
+        <p className="px-3">Profile Show</p>
+        <button className="px-6 py-1 text-white bg-blue-600" onClick={getData}>
+          Get Details
+        </button>
+        <button
+          onClick={handleLogout}
+          className="px-6 py-1 text-white bg-red-600"
+        >
+          Logout
+        </button>
+      </div>
+
+      {userData && (
+        <div className="h-40 p-10">
+          <p>
+            Name: {userData.firstName} {userData.lastName}
+          </p>
+          <p>Email: {userData.email}</p>
+          <p>Token: {userData.accessToken}</p>
+          <p>Gender:{userData.gender}</p>
+        </div>
+      )}
+      <img src={userData.image}></img>
+    </>
+  );
+};
+
+export default Show;
