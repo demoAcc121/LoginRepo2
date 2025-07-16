39257660efddf4496a88f979f1ccc8d6ad046c53
Rishi Kumar <rishichandra1415@gmail.com>
2025-07-16 22:01:46 +0530

    fix the loginPage that are never be used in it

--- src/Show.js
+++ src/Show.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import LoginPage from "./LoginPage";
 const Show = () => {
   const [userData, setUserData] = useState(null);
   const navigate = useNavigate();
